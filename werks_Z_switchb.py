6cc6cfc843a6fda8f24173a4cc459ac76835d5bc
mcedillstein <jab1983@uw.edu>
2020-05-10 01:23:15 -0700

    Updated with Mercedes SFR_flag and changed conflicting column names

--- werks_Z_switchb.py
+++ werks_Z_switchb.py
@@ -31,20 +31,18 @@ for i in columns:
 # Calculate distance
 galaxy['Distance'] = Distance(galaxy['z'])
 
-# Calculate Halpha and Hbeta Luminosity
-galaxy['Ha_luminosity'] = Ha_luminosity(galaxy['Distance'],galaxy['Halpha_flux'])
+# Calculate Halpha Luminosity, using Halpha_flux and 2.86(Hbeta_flux)
 Hbeta = 2.86*galaxy['Hbeta_flux']
-galaxy['Hb_luminosity'] = Ha_luminosity(galaxy['Distance'],Hbeta)
+galaxy['Ha_luminosity'] = Ha_luminosity(galaxy['Distance'],Hbeta)
+
+gala_Ha = Ha_luminosity(galaxy['Distance'],galaxy['Halpha_flux'])
+galaxy_Ha = gala_Ha > 0.0
+g = galaxy['Ha_luminosity']
+g[galaxy_Ha] = gala_Ha
 
 # Calculating SFR using Halpha luminosity
-galaxy['SFR_Ha'] = SFR(galaxy['Ha_luminosity'])
-
-# Calculating SFR using Hbeta luminosity, and then eliminating values (NaN) 
-# that already have 'SFR_Ha' values not equal to NaN
-sfr_b = SFR(galaxy['Hb_luminosity'])
-nosfr_b = galaxy['SFR_Ha'] > 0.0
-sfr_b[nosfr_b] = np.nan
-galaxy['SFR_Hb'] = sfr_b
+galaxy['SFR_WS'] = SFR(galaxy['Ha_luminosity'])
+galaxy['SFR_flag'] = SFR_switchboard(galaxy)
 
 # Calculate 'O3HB'
 galaxy['O3HB'] = O3HB(galaxy['OIII_flux'],galaxy['Hbeta_flux'])
@@ -106,9 +104,15 @@ one = (galaxy['Z_O3N2L'] > 0.0) | (galaxy['O3N2'] > 1.9)
 z_flag[one] = 'O3N2'
 galaxy['Z_Flag'] = z_flag
 
+# This next part is just to rename some columns that may conflict. We will name the column where
+# SFR was calculated by Werk SQuAD 'SFR' and the column where SFR was calculated using photometry (CIGALE)
+# 'SFR_photo' 
+galaxy.rename(columns={'SFR':'SFR_photo'}, inplace=True)
+galaxy.rename(columns={'SFR_WS':'SFR'}, inplace=True)
+
 # Finally we'll save the data to the directory of th eusers liking.
 txt_out = input("Please input filepath and name to save to: ")
 galaxy.to_csv(txt_out, index=False)
-print('Thank you, come again.')
+print('Thank you.')
 
 #print(galaxy)
